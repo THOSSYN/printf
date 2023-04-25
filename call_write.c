d416c5a5af124930b71433d2d01f3b10ea61903d
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-25 15:09:12 +0000

    read and write a buffer

--- call_write.c
+++ call_write.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
 
 int main(void)
 {
+	int j;
+
 	char buffer[BUFFER_SIZE];
 
-	return (write(1, buffer, BUFFER_SIZE));
+	j = read(0, buffer, BUFFER_SIZE);
+	
+	return (write(1, buffer, j));
 }
