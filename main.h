d32c2b411e1add4325f620f2d81ae3f4ea30446b
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-25 13:50:50 +0000

    Update header file and functions

--- main.h
+++ main.h
@@ -9,6 +9,9 @@ int _printf(const char *format, ...);
 size_t len(char *str);
 int int_out(int first_arg, char sign, int base, char *buffer);
 int _vprintf(const char *format, va_list first_arg);
-
+int iTB(unsigned int num);
+int iTO(unsigned int num);
+int iTH(unsigned int num);
+int iTh(unsigned int num);
 
 #endif /* MAIN_H */
--- methods.c
+++ methods.c
@@ -55,3 +55,119 @@ int int_out(int first_arg, char sign, int base, char *buffer)
 		_putchar(buffer[j]);
 	return(i);
 }
+
+/**
+ * iTh - Convert decimal to hexadecimal
+ * @num: number to be converted
+ *
+ * Return: returns an integer
+ */
+int iTh(unsigned int num)
+{
+	char buffer[33];
+	int b;
+	unsigned int temp;
+	
+	int i = 0;
+	while (num != 0)
+	{
+		temp = num % 16;
+		
+		if (temp < 10)
+		{
+			buffer[i] = temp + 48;
+			i++;
+		} else
+		{
+			buffer[i] = temp + 55;
+			i++;
+		}
+		num /= 16;
+	}
+	
+	for (b = i - 1; b >=0; b--)
+		_putchar(buffer[b]);
+	return (i);
+}
+
+/**
+ * iTH - Convert decimal to Hexadecimal
+ * @num: number to be converted
+ *
+ * Return: returns an integer
+ */
+int TH(unsigned int num)
+{
+	char buffer[33];
+	int b;unsigned int temp;
+	
+	int i = 0;
+	while (num != 0)
+	{
+		int rem = 0;
+		
+		temp = num % 16;
+		
+		if (temp < 10)
+		{
+			buffer[i] = temp + 48;
+			i++;
+		} else
+		{
+			buffer[i] = temp + 87;
+			i++;
+		}
+		num /= 16;
+	}
+	for (b = i - 1; b >=0; b--)
+		_putchar(buffer[b]);
+	return (i);
+}
+/**
+ * iTO - Convert decimal to Hexadecimal
+ * @num: number to be converted
+ *
+ * Return: returns an integer
+ */
+int iTO(unsigned int num)
+{
+	char buffer[33];
+	unsigned int rem = 0;
+	int b, i;
+	
+	i = 0;
+	while (num != 0 && i < sizeof(buffer))
+	{
+		rem = num % 8;
+		buffer[i] = rem + 48;
+		num /= 8;
+		i++;
+	}
+	for (b = i - 1; b >= 0; b--)
+		_putchar(buffer[b]);
+	return (i);
+}
+/**
+ * iTB - Convert decimal to Hexadecimal
+ * @num: number to be converted
+ *
+ * Return: returns an integer
+ */
+int iTB(unsigned int num)
+{
+	char buffer[33];
+	unsigned int rem = 0;
+	int b, i;
+	
+	i = 0;
+	while (num != 0 && i < sizeof(buffer))
+	{
+		rem = num % 2;
+		buffer[i] = rem + 48;
+		num /= 2;
+		i++;
+	}
+	for (b = i - 1; b >= 0; b--)
+		_putchar(buffer[b]);
+	return (i);
+}
